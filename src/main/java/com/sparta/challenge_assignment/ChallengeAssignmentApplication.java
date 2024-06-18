1a707f48f2ab69cfac3ee28456e0adfc8afb79c7
seongmo-yoon <momoysm@gmail.com>
2024-06-18 10:22:28 +0900

    git init

---
+++ src/main/java/com/sparta/challenge_assignment/ChallengeAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.challenge_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/challenge_assignment/ChallengeAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.challenge_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
